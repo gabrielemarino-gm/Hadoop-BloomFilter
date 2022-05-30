1942c044c55a125882e5ce454c9e42a4d73a7d6b
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2022-05-30 20:02:23 +0200

    Update BloomFilter.java

--- BloomFilterMapReduce/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ BloomFilterMapReduce/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -1,11 +1,15 @@
 package it.unipi.hadoop;
 
+import it.unipi.hadoop.BloomFilterMR;
+import it.unipi.hadoop.ConstructionMR;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -13,30 +17,39 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.util.hash.Hash;
 import org.apache.hadoop.util.hash.MurmurHash;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
-public class BloomFilter {
+public class BloomFilter
+{
     private static final int NUM_BLOOM_FILTERS = 10;
     private static final int k = 7;
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception
+    {
+        testSet();
+
+
         long start = System.currentTimeMillis();
 
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
-        if (otherArgs.length != 2) {
+        if (otherArgs.length != 2)
+        {
             System.err.println("Usage: BloomFilter <input path> <output path> ");
             System.exit(-1);
         }
 
-        if (!constructionJob(conf, otherArgs[0], "tmp1"))
+        if (!constructionJob(conf, otherArgs[0], "tmp1")) // Here we wait for the execution of the first MapReduce algorithm
             System.exit(-1);
 
-        boolean finalStatus = !bloomFilterJob(conf, "tmp1", otherArgs[0], otherArgs[1]);
+        boolean finalStatus = !bloomFilterJob(conf, "tmp1/part-r-00000", otherArgs[0], otherArgs[1]);
         //removeDirectory(conf, "tmp2");
 
         // TIME
@@ -47,15 +60,35 @@ public class BloomFilter {
         if (!finalStatus)
             System.exit(-1);
 
-        System.out.println("TESTING THE FALSE POSITIVE RATES");
-        testJob(conf, otherArgs[1]);
+        // System.out.println("TESTING THE FALSE POSITIVE RATES");
+        // testJob(conf, otherArgs[1]);
 
     }
 
-    private static boolean constructionJob(Configuration conf, String inPath, String outPath) throws Exception {
 
+    private static void testSet()
+    {
+        System.out.println();
+        System.out.println();
+        int[] bloomFilter = new int[16];
+        for(int i=0; i<16; i++)
+        {
+            bloomFilter[i] = i;
+        }
+
+        IntArrayWritable test = new IntArrayWritable();
+        test.set(bloomFilter);
+        System.out.println("TEST STAMPA INT_ARRAY_WRITABLE");
+        System.out.println("TEST : " + test.toString());
+        System.out.println();
+        System.out.println();
+
+    }
+
+    private static boolean constructionJob(Configuration conf, String inPath, String outPath) throws Exception
+    {
         Job job = Job.getInstance(conf, "ConstructionMR");
-        job.setJarByClass(ConstructionMR.class);
+        job.setJarByClass(BloomFilter.class);
 
         // set mapper/reducer
         job.setMapperClass(ConstructionMR.ConstructionMapper.class);
@@ -73,7 +106,7 @@ public class BloomFilter {
         job.setInputFormatClass(NLineInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        //FileInputFormat.addInputPath(job, new Path(inPath));
+        // FileInputFormat.addInputPath(job, new Path(inPath));
         NLineInputFormat.addInputPath(job, new Path(inPath));
         job.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", 311782);
         FileOutputFormat.setOutputPath(job, new Path(outPath));
@@ -81,7 +114,8 @@ public class BloomFilter {
         return job.waitForCompletion(true);
     }
 
-    private static boolean bloomFilterJob(Configuration conf, String inDataPath, String inPath, String outPath) throws Exception {
+    private static boolean bloomFilterJob(Configuration conf, String inDataPath, String inPath, String outPath) throws Exception
+    {
         int[] m = readM(conf, inDataPath, "");
         conf.setInt("m_1", m[0]);
         conf.setInt("m_2", m[1]);
@@ -93,8 +127,49 @@ public class BloomFilter {
         conf.setInt("m_8", m[7]);
         conf.setInt("m_9", m[8]);
         conf.setInt("m_10", m[9]);
+
+        //  ------------------------------- TEST --------------------------- //
+        /*System.out.println();
+        System.out.println();
+
+        System.out.println("TEST HASH");
+        int mario = 0;
+        int index = (int) Math.round((7.5));
+
+        try
+        {
+            mario = Integer.parseInt(conf.get("m_" + index));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error in parsing the input file");
+        }
+
+        System.out.println("Trovata per 8, m = " + mario);
+        String movie_name = "tt123456789";
+        // take m from the configuration
+        Hash h = new MurmurHash();
+
+        System.out.println("Lunghezza nome :" + movie_name.length());
+        System.out.println("Lunghezza byte :" + movie_name.getBytes(StandardCharsets.UTF_8).length);
+        int myHash = h.hash(movie_name.getBytes(StandardCharsets.UTF_8), movie_name.length(), 1);
+        System.out.println("Senza modulo h(" + movie_name + ") = " + myHash);
+
+        // (a % b + b) % b
+        myHash = (myHash%mario + mario) % mario;
+        System.out.println("h(" + movie_name + ") = " + myHash);
+        System.out.println();
+        System.out.println();*/
+        //  ------------------------------- END TEST --------------------------- //
+
+
+
         Job job = Job.getInstance(conf, "BloomFilterMR");
         //job.setJarByClass(InMemoryMovingAverage.class);
+        // System.out.println("M1 = " + conf.get("m_1"));
+
+
+        job.setJarByClass(BloomFilter.class);
 
         // set mapper/reducer
         job.setMapperClass(BloomFilterMR.BloomFilterMapper.class);
@@ -102,13 +177,13 @@ public class BloomFilter {
 
         // define mapper's output key-value
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntArrayWritable.class);
+        job.setMapOutputValueClass(BloomFilter.IntArrayWritable.class);
 
         // define reducer's output key-value
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        //job.setOutputValueClass(Text.class);
         //alternative: the output is an array of int
-        //job.setOutputValueClass(IntArrayWritable.class);
+        job.setOutputValueClass(BloomFilter.IntArrayWritable.class);
 
         // define I/O
         //job.setInputFormatClass(TextInputFormat.class);
@@ -123,60 +198,81 @@ public class BloomFilter {
         return job.waitForCompletion(true);
     }
 
-    private static void testJob(Configuration conf, String inDataPath) throws IOException {
-        //TODO 30/05/2022: test the bloom filter creation, then get the bloom filters and calculate the false postive rates
+    /*
+    private static void testJob(Configuration conf, String inDataPath) throws IOException
+    {
+        // TODO 30/05/2022: Test the bloom filter creation, then get the bloom filters and calculate the false postive rates
         FileSystem hdfs = FileSystem.get(conf);
         double falsePositives[] = new double[10];
         double trueNegatives [] = new double[10];
-        BufferedReader br= new BufferedReader(new InputStreamReader(hdfs.open(new Path(inDataPath))));
-        try {
+        BufferedReader br = new BufferedReader(new InputStreamReader(hdfs.open(new Path(inDataPath))));
+
+        try
+        {
             String line;
-            line=br.readLine();
-            while (line != null){
+            line = br.readLine();
+
+            while (line != null)
+            {
                 String[] inputs = line.split("\t");
                 int i = Integer.parseInt(inputs[0]);
-                for(int j = 0; j < k; j++){
-                    //TODO 30/05/2022: apply the hash functions to find the position of the element in the filter
+                for(int j = 0; j < k; j++)
+                {
+                    // TODO 30/05/2022: Apply the hash functions to find the position of the element in the filter
 
                 }
+
                 // be sure to read the next line otherwise we get an infinite loop
                 line = br.readLine();
             }
-        } finally {
+        }
+        finally
+        {
             // close out the BufferedReader
             br.close();
         }
-    }
+    }*/
     
-    private static int[] readM(Configuration conf, String pathString, String pattern) throws Exception {
-        int result[] = new int[NUM_BLOOM_FILTERS];
+    private static int[] readM(Configuration conf, String pathString, String pattern) throws Exception
+    {
+        int[] result = new int[NUM_BLOOM_FILTERS];
         FileSystem hdfs = FileSystem.get(conf);
 
         BufferedReader br= new BufferedReader(new InputStreamReader(hdfs.open(new Path(pathString))));
-        try {
+
+        try
+        {
             String line;
             line=br.readLine();
-            while (line != null){
-                if (line.startsWith(pattern)) {
+
+            while (line != null)
+            {
+                if (line.startsWith(pattern))
+                {
                     String[] inputs = line.split("\t");
                     int i = Integer.parseInt(inputs[0]);
                     result[i-1] = Integer.parseInt(inputs[1]);
+                    // System.out.println("Result: " + result[i-1]);
                     //break;
                 }
 
                 // be sure to read the next line otherwise we get an infinite loop
                 line = br.readLine();
             }
-        } finally {
+        }
+        finally
+        {
             // close out the BufferedReader
             br.close();
         }
+
 
         return result;
     }
 
     /*********************UTILS************************/
-    public static class IntArrayWritable extends ArrayWritable {
+    public static class IntArrayWritable extends ArrayWritable
+    {
 
         public IntArrayWritable() {
             super(IntWritable.class);
@@ -187,37 +283,69 @@ public class BloomFilter {
         }
 
         @Override
-        public IntWritable[] get() {
-            return (IntWritable[]) super.get();
+        public IntWritable[] get()
+        {
+            Writable[] tmp = super.get();
+            if(tmp != null)
+            {
+                int n = tmp.length;
+                IntWritable[] items = new IntWritable[n];
+                for(int i = 0; i < tmp.length; i++)
+                {
+                    items[i] = (IntWritable)tmp[i];
+                }
+                return items;
+            }
+            else
+            {
+                return null;
+            }
+            //return (IntWritable[]) super.get();
         }
 
-        public int[] getData() {
-            IntWritable data [] = (IntWritable[]) super.get();
-            int result[] = new int[data.length];
-            for(int i = 0; i < data.length; i++){
+
+        public int[] getData()
+        {
+            IntWritable[] data = (IntWritable[]) super.get();
+            int[] result = new int[data.length];
+            for(int i = 0; i < data.length; i++)
+            {
                 result[i] = data[i].get();
             }
+
             return result;
         }
-        public void set(int[] array){
-            IntWritable values[] = new IntWritable[array.length];
-            for (int i = 0; i < array.length; i++){
-                values[i].set(array[i]);
+
+        public void set(int[] array)
+        {
+            IntWritable[] values = new IntWritable[array.length];
+
+            for (int i=0; i<array.length; i++)
+            {
+                int temp = array[i];
+                values[i] = new IntWritable();
+                values[i].set(temp);
             }
+
             super.set(values);
         }
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             IntWritable[] values = (IntWritable[]) super.get();
-            if(values.length == 0){
+            if(values.length == 0)
+            {
                 return "";
             }
+
             StringBuilder sb  = new StringBuilder();
-            for(IntWritable value : values){
+            for(IntWritable value : values)
+            {
                 int i = value.get();
                 sb.append(i).append(" ");
             }
+
             sb.setLength(sb.length()-1);
             return sb.toString();
             //return values[0].toString() + ", " + values[1].toString();
