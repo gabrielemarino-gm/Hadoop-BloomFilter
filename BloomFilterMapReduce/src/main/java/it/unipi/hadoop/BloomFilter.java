14733977a802a26c49d9985bd8508dbc9f252042
Stepnod98 <stefanodugo1998@gmail.com>
2022-05-31 17:01:04 +0200

    removed print

--- BloomFilterMapReduce/src/main/java/it/unipi/hadoop/BloomFilter.java
+++ BloomFilterMapReduce/src/main/java/it/unipi/hadoop/BloomFilter.java
@@ -200,8 +200,6 @@ public class BloomFilter
                         //take the hash value for chekcking the elements
                         int pos = (h.hash(movie_name.getBytes(StandardCharsets.UTF_8), movie_name.length(), j) % m[i] + m[i]) % m[i];
                        
-                        if(l == 0 && j == 0)
-                            System.out.println("Hash value: " + pos);
                         String[] elements = bloomFilter[l].split(" ");
                         //if there is not an element but it's not supposed to be there, then the element is a true negative
                         if(elements.length < pos){
